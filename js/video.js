356098838c0ccf735a44c3db5183f7882c3b5193
gatfield <gatfield@umich.edu>
2024-03-31 20:04:09 -0400

    commit

--- js/video.js
+++ js/video.js
@@ -11,6 +11,7 @@ window.addEventListener("load", function() {
 
 var play = document.getElementById("play");
 play.addEventListener("click", function() {
+	video = document.getElementById("player1");
 	video.play();
 	// udpdate volume
 	document.getElementById("volume").innerHTML = video.volume + "%";
@@ -18,17 +19,20 @@ play.addEventListener("click", function() {
 
 var pause = document.getElementById("pause");
 pause.addEventListener("click", function() {
+	video = document.getElementById("player1");
 	video.pause();
 });
 
 var slower = document.getElementById("slower");
 slower.addEventListener("click", function() {
+	video = document.getElementById("player1");
 	video.playbackRate *= 0.90;
 	console.log("The new video speed is " + video.playbackRate);
 });
 
 var faster = document.getElementById("faster");
 faster.addEventListener("click", function() {
+	video = document.getElementById("player1");
 	video.playbackRate /= 0.90;
 	console.log("The new video speed is " + video.playbackRate);
 });
@@ -36,6 +40,7 @@ faster.addEventListener("click", function() {
 // Advance the current video by 10 seconds.  If the video length has been exceeded go back to the start of the video - no farther.   Log the current location of the video.
 var skip = document.getElementById("skip");
 skip.addEventListener("click", function() {
+	video = document.getElementById("player1");
 	if (video.currentTime + 10 < video.duration) {
 		video.currentTime += 10;
 	} else {
@@ -47,11 +52,12 @@ skip.addEventListener("click", function() {
 // Mute/unmute the video and update the text in the button.
 var mute = document.getElementById("mute");
 mute.addEventListener("click", function() {
+	video = document.getElementById("player1");
 	if (video.muted == true) {
 		video.muted = false;
 		mute.innerHTML = "Mute";
 	} else {
-		video.volume = 0;
+		video.muted = true;
 		mute.innerHTML = "Unmute";
 	}
 });
@@ -59,6 +65,7 @@ mute.addEventListener("click", function() {
 //Change the volume based on the slider and update the volume information.
 var slider = document.getElementById("slider");
 slider.addEventListener("change", function() {
+	video = document.getElementById("player1");
 	console.log(this.value);
 	video.volume = this.value;
 	document.getElementById("volume").innerHTML = video.volume + "%";
@@ -67,11 +74,13 @@ slider.addEventListener("change", function() {
 // Utilize the existing oldSchool class on the video element
 var vintage = document.getElementById("vintage");
 vintage.addEventListener("click", function() {
+	video = document.getElementById("player1");
 	video.classList.add("oldSchool");
 });
 
 // Remove the oldSchool class from the video.
 var orig = document.getElementById("orig");
 orig.addEventListener("click", function() {
+	video = document.getElementById("player1");
 	video.classList.remove("oldSchool");
 });
