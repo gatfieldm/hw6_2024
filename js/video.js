97d1d19a00e41e1ed550c383f91824c212f059ae
gatfield <gatfield@umich.edu>
2024-03-31 20:02:35 -0400

    commit

--- js/video.js
+++ js/video.js
@@ -11,7 +11,6 @@ window.addEventListener("load", function() {
 
 var play = document.getElementById("play");
 play.addEventListener("click", function() {
-	console.log("Play Video");
 	video.play();
 	// udpdate volume
 	document.getElementById("volume").innerHTML = video.volume + "%";
@@ -19,20 +18,17 @@ play.addEventListener("click", function() {
 
 var pause = document.getElementById("pause");
 pause.addEventListener("click", function() {
-	console.log("Pause Video");
 	video.pause();
 });
 
 var slower = document.getElementById("slower");
 slower.addEventListener("click", function() {
-	console.log("Slow Down Video");
 	video.playbackRate *= 0.90;
 	console.log("The new video speed is " + video.playbackRate);
 });
 
 var faster = document.getElementById("faster");
 faster.addEventListener("click", function() {
-	console.log("Speed Up Video");
 	video.playbackRate /= 0.90;
 	console.log("The new video speed is " + video.playbackRate);
 });
@@ -40,9 +36,8 @@ faster.addEventListener("click", function() {
 // Advance the current video by 10 seconds.  If the video length has been exceeded go back to the start of the video - no farther.   Log the current location of the video.
 var skip = document.getElementById("skip");
 skip.addEventListener("click", function() {
-	console.log("Skip Ahead");
-	if (video.currentTime + 15 < video.duration) {
-		video.currentTime += 15;
+	if (video.currentTime + 10 < video.duration) {
+		video.currentTime += 10;
 	} else {
 		video.currentTime = 0;
 		video.play();
